d3603bd22af0c907f0c4d546155b95f5215929a9
uri paz <uripaz9@gmail.com>
2019-04-25 11:39:19 +0300

    Fitst commit
    Some description

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello World</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
